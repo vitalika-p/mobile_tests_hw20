56850d127fb725c5e2ee2d5bd91aad085d1cb17d
Vitalika <vitamin@MacBook-Air-Vitalika.local>
2025-01-26 19:19:13 +0300

    add .corrections

--- src/test/java/screens/SearchScreen.java
+++ src/test/java/screens/SearchScreen.java
@@ -1,21 +1,54 @@
 package screens;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SearchScreen {
-    private final SelenideElement searchCard = $(accessibilityId("Search Wikipedia")),
-        searchBar = $(id("org.wikipedia.alpha:id/search_src_text"));
+    private final SelenideElement searchCard = $(accessibilityId("Search Wikipedia"));
+    private final SelenideElement searchBar = $(id("org.wikipedia.alpha:id/search_src_text"));
+    private final ElementsCollection searchKeyWords = $$(id("org.wikipedia.alpha:id/page_list_item_title"));
+    public final SelenideElement clearButton = $(id("org.wikipedia.alpha:id/search_close_btn"));
 
-    @Step("Ввести поисковый запрос")
-    public SearchScreen enterSearchQuery(String query) {
+
+    @Step("Открыть поиск")
+    public SearchScreen openSearchPage() {
         searchCard.click();
-        searchBar.sendKeys(query);
-
         return this;
+    }
+    @Step("Ввести валидный поисковый запрос")
+    public SearchScreen enterValidQuery(String query) {
+        searchBar.sendKeys(query);
+        return this;
+    }
+
+    @Step("Открыть первый результат")
+    public SearchScreen openFirstSearchResult() {
+        searchKeyWords.first().click();
+        return this;
+    }
+
+    @Step("Ввести невалидный поисковый запрос")
+    public SearchScreen enterInvalidQuery(String query) {
+        searchBar.sendKeys(query);
+        return this;
+    }
+
+    @Step("Нажать на крестик для очистки поля поиска")
+    public SearchScreen clearSearchField() {
+        clearButton.click();
+        return this;
+    }
+
+    @Step("Проверить, что поле поиска пустое")
+    public boolean isSearchFieldEmpty() {
+        return searchBar.getText().isEmpty();
     }
 }
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -3,27 +3,32 @@ package tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import screens.SearchScreen;
 
 import javax.management.Query;
 
+import static com.codeborne.selenide.Selenide.$;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class SearchTests extends TestBase {
+    screens.SearchScreen searchScreen = new SearchScreen();
 
     @Test
     @Tag("android")
-    @DisplayName("Ввести валидный поисковый запрос в строку поиска")
+    @DisplayName("Открытие первого валидного результата")
     void successfulSearchTest() {
-        screens.SearchScreen searchPage = new screens.SearchScreen();
-
-        searchPage.enterSearchQuery("QA Engineer");
+        searchScreen.openSearchPage();
+        searchScreen.enterValidQuery("QA Engineer");
+        searchScreen.openFirstSearchResult();
     }
 
     @Test
     @Tag("android")
-    @DisplayName("Ввести невалидный поисковый запрос в строку поиска")
-    void unSuccessfulSearchTest() {
-        screens.SearchResultsScreen searchPage = new screens.SearchResultsScreen();
-
-        searchPage.enterInvalidQuery("slhjhfkshj");
+    @DisplayName("Очистка поля ввода после тапа на крестик")
+    void clearSearchFieldTest() {
+        searchScreen.openSearchPage();
+        searchScreen.enterInvalidQuery("!@#$%^&*");
+        searchScreen.clearSearchField();
+        searchScreen.isSearchFieldEmpty();
     }
-
 }
